b54b3074d0c8961e99808f5fc86f19629c39150a
Omkar--p <op8775@gmail.com>
2022-09-27 18:13:56 +0530

    first commit

--- src/component/HeadEvent.js
+++ src/component/HeadEvent.js
@@ -13,9 +13,7 @@ export default function HeadEvent() {
         <div className='text-center col-lg-6 border border-5 container-lg p-4'  style={{height:300}}>
             <p>First Event </p>
         </div>
-        <div className='text-center col-lg-6 border border-5 container-lg p-4'  style={{height:300}}>
-            <p>Second Event</p>
-        </div>
+        
     </div>
     </div>
   )
